cd93b3bc32bb603f2d055d292cb80adcef1076d4
varsha2009 <72211540+varsha2009@users.noreply.github.com>
2021-05-05 12:07:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(400,350);
+    canvas.position(485,245);
+}
+function draw(){
+}
+function take_snapshot(){
+    save("my_inage.png");
+}
